03b069dbb0bff68ea14d402939dcab562535762e
foreverTuesday <scheffm@simmons.edu>
2017-05-05 21:50:58 -0400

    Add files via upload

--- CS330/MenuViewController.swift
+++ CS330/MenuViewController.swift
@@ -15,28 +15,43 @@ class MenuViewController: UIViewController {
     //GlobalVariable.myString = "Hello"
         ViewController.GlobalVariable.safeCheck = false
         
-        var buttonArray = [toggleUp1,toggleUp2] //create button outlet array
-        showUpgrades(ar: buttonArray)
+        var buttonArray = [toggleUp1,toggleUp2,toggleUp3,toggleUp4] //create button outlet array
+        //showUpgrades(ar: buttonArray)
+        
+
     // Do any additional setup after loading the view.
+        
 
     //print("I got to viewDidLoad in MenuViewController")
     
         GlobalVariable.tempBank = ViewController.GlobalVariable.inBank //put the right number into tempBank
         bankLabel.text = "Bank: \(GlobalVariable.tempBank)"  //display the right number in bankLabel
 
+        var counter = 0
         for i in GlobalVariable.checkUpgrades{  //check if upgrades have been used
-            var counter = 0
             if i == true{
-                buttonArray[counter]?.isHidden=i  //hide all upgrades that have been used
+                buttonArray[counter]!.isHidden=true  //hide all upgrades that have been used
             }
             counter = counter + 1
         }
-     
+
+        initiateTimer()  //start checking the timer to see if bankLabel needs to be updated
+            //print("The timer exists!!!!!!")
+        
+        
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    override func viewWillDisappear(_ animated: Bool){ //function that gets called when the view disappears
+        littleTimer.invalidate()
+        
+        //print("MVC vanished!")
     }
     
 //variables
@@ -48,6 +63,7 @@ class MenuViewController: UIViewController {
     
 //show only the unused upgrade buttons
     func showUpgrades(ar: Array<Any>){
+        //the code I wanted her is in viewDidLoad, it throws errors when moved here
             }
     
     
@@ -58,8 +74,8 @@ class MenuViewController: UIViewController {
     
 //first upgrade - double click power
     @IBAction func up1(_ sender: AnyObject) {
-        if (GlobalVariable.tempBank>=10){ //if you have enough to buy the upgrade
-            GlobalVariable.tempBank = GlobalVariable.tempBank - 10 //pay the cost
+        if (GlobalVariable.tempBank>=100){ //if you have enough to buy the upgrade
+            GlobalVariable.tempBank = GlobalVariable.tempBank - 100 //pay the cost
             ViewController.GlobalVariable.clickMultiplier = ViewController.GlobalVariable.clickMultiplier*2//update multiplier
             upd8Banks()  //fix what the bank is displaying
             toggleUp1.isHidden = true  //hide button
@@ -68,16 +84,45 @@ class MenuViewController: UIViewController {
     }
     @IBOutlet weak var toggleUp1: UIButton!
     
-//second upgrade -
+//second upgrade - free coin every 10 seconds
     @IBAction func up2(_ sender: AnyObject) {
 //when the upgrade is bought, start the timer
-        ViewController.GlobalVariable.initiateTimer() //initiate idle part of the game
-        
-        
-        toggleUp2.isHidden = true  //hide button
-        GlobalVariable.checkUpgrades[1]=true //update checkUpgrades to show that upgrade has been used
+        if (GlobalVariable.tempBank>=250){ //if you have enough to buy the upgrade
+            GlobalVariable.tempBank = GlobalVariable.tempBank - 250 //pay the cost
+            ViewController.GlobalVariable.initiateTimer() //initiate idle part of the game
+            upd8Banks()  //fix what the bank is displaying
+            toggleUp2.isHidden = true  //hide button
+            GlobalVariable.checkUpgrades[1]=true //update checkUpgrades to show that upgrade has been used
+        }
     }
     @IBOutlet weak var toggleUp2: UIButton!
+    
+//third upgrade- click power +1
+    @IBAction func up3(_ sender: AnyObject) {
+        if (GlobalVariable.tempBank>=500){ //if you have enough to buy the upgrade
+            GlobalVariable.tempBank = GlobalVariable.tempBank - 500 //pay the cost
+            ViewController.GlobalVariable.clickMultiplier = ViewController.GlobalVariable.clickMultiplier+1//update multiplier
+            upd8Banks()  //fix what the bank is displaying
+            toggleUp3.isHidden = true  //hide button
+            GlobalVariable.checkUpgrades[2]=true //update checkUpgrades to show that upgrade has been used
+        }
+    }
+    @IBOutlet weak var toggleUp3: UIButton!
+    
+    
+//fourth upgrade
+    @IBAction func up4(_ sender: AnyObject) {
+        //when the upgrade is bought, start the timer
+        if (GlobalVariable.tempBank>=500){ //if you have enough to buy the upgrade
+            GlobalVariable.tempBank = GlobalVariable.tempBank - 500 //pay the cost
+            ViewController.GlobalVariable.initiateTimer2() //initiate idle part of the game
+            upd8Banks()  //fix what the bank is displaying
+            toggleUp4.isHidden = true  //hide button
+            GlobalVariable.checkUpgrades[3]=true //update checkUpgrades to show that upgrade has been used
+        }
+    }
+    @IBOutlet weak var toggleUp4: UIButton!
+    
     
     
 //update the tempBank and real bank, amounts and labels
@@ -85,6 +130,32 @@ class MenuViewController: UIViewController {
         bankLabel.text = "Bank: \(GlobalVariable.tempBank)"  //update text
 
     }
+    
+ 
+//check safeStore once per second and update bankLabel if needed
+    var littleTimer = Timer()
+    var timeCounter = 0
+    var checker = 0
+    
+    func initiateTimer(){
+        littleTimer = Timer.scheduledTimer(timeInterval: 1, target:self, /*selector: Selector("checkTimer")*/selector: #selector(checkTimer), userInfo: nil, repeats: true)
+    }
+
+    
+    func checkTimer() {
+        //timeCounter =  timeCounter + 1  //increment the counter
+        if ViewController.GlobalVariable.safeStore>checker/*ViewController.GlobalVariable.safeStore>0*/{
+            GlobalVariable.tempBank = GlobalVariable.tempBank + 1
+            upd8Banks()
+            checker = ViewController.GlobalVariable.safeStore
+        }
+        //print(GlobalVariable.tempBank)
+        
+    }
+
+    
+    
+    
     
     
 //update Game State
@@ -99,13 +170,13 @@ class MenuViewController: UIViewController {
 //Global Variables
     struct GlobalVariable{
         //static var myString = String()
-        static var checkUpgrades: Array<Bool>=[false,false]
-        static var tempBank = Int()
+        static var checkUpgrades: Array<Bool>=[false,false,false,false]
+        static var tempBank = 0
         
    //timer variables
         
         
-        static var debugNum = 0
+        //static var debugNum = 0
 
     }
 
--- CS330/ViewController.swift
+++ CS330/ViewController.swift
@@ -13,10 +13,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        navigationController?.navigationBar.barTintColor = UIColor.black
+        navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.orange]
+
+
         GlobalVariable.clickMultiplier = 1
         //print(MenuViewController.GlobalVariable.myString)
-        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.bankText), name: NSNotification.Name(rawValue: "UpdateBankText"), object: nil)
             //print("I got to viewDidLoad")
+        let title = "Instructions"
+        let message = "Welcome to coaster clicker.  The goal of the game is to get as many coins as possible.  Every time you click the big coin, you get one coin.  In the menu, you can buy upgrades to help you get coins faster.  Click \"Start\" when you are ready to begin."
+        let alertView: UIAlertView = UIAlertView(title: title, message: message, delegate: self, cancelButtonTitle: "Start")
+        // alertView.alertViewStyle = .default
+        alertView.show()
+
+    }
+    
+    func alertView(_ alertView: UIAlertView, clickedButtonAtIndex buttonIndex: Int) {
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,26 +41,43 @@ class ViewController: UIViewController {
         super.viewWillAppear(animated)
         
         //let tempBank = MenuViewController.GlobalVariable.tempBank + GlobalVariable.safeStore
-        //updateBank(num: tempBanK)
+        //updateBank(num: tempBank)
         
         updateBank(num: MenuViewController.GlobalVariable.tempBank)
         
         GlobalVariable.safeCheck = true
-        //GlobalVariable.safeStore = 0
+        GlobalVariable.safeStore = 0
         //print("I got to viewWillAppear")
     }
+   
     
+    
+    
+    override func viewWillDisappear(_ animated: Bool){  //function that gets called when the view disappears
+        //print("VC vanished!")
+    }
+
 //variables
     var clicks = 0
-    
+    //var secondClick = false
     
 //main button
     @IBAction func mainButton(_ sender: AnyObject) {
     //update overall clicks
-        clicks = clicks + 1
+        clicks += 1
+        GlobalVariable.inBank = GlobalVariable.inBank + (GlobalVariable.clickMultiplier)
+       //hard mode where a user must click twice for one coin
+        /*if secondClick==true{
+            clicks += 1/*clicks + 1*/
+            secondClick = false
+            GlobalVariable.inBank = GlobalVariable.inBank + (GlobalVariable.clickMultiplier)
+        }
+        else{
+            secondClick=true
+        }*/
         clickCount.text = "Number of clicks: \(clicks)"
     //update bank by adding the correct number of coins
-        GlobalVariable.inBank = GlobalVariable.inBank + (GlobalVariable.clickMultiplier)
+        
         bankText()
 
         //GlobalVariable.test()
@@ -71,9 +101,10 @@ class ViewController: UIViewController {
         clickBank.text = "Bank: \(GlobalVariable.inBank)"
     }
 
-//timer
+//timers
+    //first upgrade timer
     static func initiateTimer(){
-        GlobalVariable.SwiftTimer = Timer.scheduledTimer(timeInterval: 1, target:self, selector: Selector("updateCounter")/*selector: #selector(ViewController.GlobalVariable.updateCounter)*/, userInfo: nil, repeats: true)
+        GlobalVariable.SwiftTimer = Timer.scheduledTimer(timeInterval: 10, target:self, selector: Selector("updateCounter")/*selector: #selector(ViewController.GlobalVariable.updateCounter)*/, userInfo: nil, repeats: true)
     }
     
     static func updateCounter() {
@@ -81,14 +112,14 @@ class ViewController: UIViewController {
         //print(GlobalVariable.SwiftCounter)
         GlobalVariable.inBank = GlobalVariable.inBank + 1
         if (GlobalVariable.safeCheck == true){
-            print("Safecheck is true")
-            ViewController().bankText()
+            //print("Safecheck is true")
+            NotificationCenter.default.post(name: Notification.Name("UpdateBankText"), object: nil)  //update the bank label
+            //ViewController().bankText()
         }
         else{
             //print("Safecheck is false")
             GlobalVariable.safeStore = GlobalVariable.safeStore + 1  //keep track of how many coins need to be added
-            print("Safestore = \(GlobalVariable.safeStore)")
-            //ViewController().updateSafeStore()
+            //print("Safestore = \(GlobalVariable.safeStore)")
         }
     }
 
@@ -98,12 +129,32 @@ class ViewController: UIViewController {
         safeStore = safeStore + 1
         print("safeStore: \(safeStore)")
     }*/
+ //second upgrade timer
+    static func initiateTimer2(){
+        GlobalVariable.SwiftTimer2 = Timer.scheduledTimer(timeInterval: 5, target:self, selector: Selector("updateCounter2")/*selector: #selector(ViewController.GlobalVariable.updateCounter)*/, userInfo: nil, repeats: true)
+    }
+    
+    static func updateCounter2() {
+        GlobalVariable.SwiftCounter2 =  GlobalVariable.SwiftCounter2 + 1  //increment the counter
+        //print(GlobalVariable.SwiftCounter)
+        GlobalVariable.inBank = GlobalVariable.inBank + 1
+        if (GlobalVariable.safeCheck == true){
+            //print("Safecheck is true")
+            NotificationCenter.default.post(name: Notification.Name("UpdateBankText"), object: nil)  //update the bank label
+            //ViewController().bankText()
+        }
+        else{
+            //print("Safecheck is false")
+            GlobalVariable.safeStore = GlobalVariable.safeStore + 1  //keep track of how many coins need to be added
+            //print("Safestore = \(GlobalVariable.safeStore)")
+        }
+    }
 
 
 //function for debugging
-    func debug(){
+   /* func debug(){
         print("I got to the debug function")
-    }
+    }*/
 
     
 
@@ -116,14 +167,21 @@ class ViewController: UIViewController {
             //update inBank and the bank label
         }*/
         
-    //timer
+    //timers
         static var SwiftTimer = Timer()
         static var SwiftCounter = 0
         static func initiateTimer(){
             ViewController.initiateTimer()
             
             /*SwiftTimer = Timer.scheduledTimer(timeInterval: 1, target:self, selector: Selector("updateCounter")/*selector: #selector(updateCounter)*/, userInfo: nil, repeats: true)*/
+            
         }
+        static var SwiftTimer2 = Timer()
+        static var SwiftCounter2 = 0
+        static func initiateTimer2(){
+            ViewController.initiateTimer2()
+        }
+        
     //how to handle timer stuff while in MenuViewController
         static var safeCheck = Bool() //so that bankText doesn't get run while in MenuViewController
         static var safeStore = Int()
